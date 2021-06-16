2777d0bb9341f21ae1cd63c21cf31227ec9380db
Corentin Banier <corentin.banier@etu.u-bordeaux.fr>
2021-06-16 12:19:19 +0200

    First version

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
 import os, random
 import discord
-from discord import channel
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord.ext.commands import context
 from dotenv import load_dotenv
+from itertools import cycle
 
 bot = commands.Bot(command_prefix='!')
+status = cycle(['Boulie Manitas', "Type '!help' for help"])
 
 @bot.event
 async def on_ready():
-	print(f'Bot connected as {bot.user}')
+    change_status.start()
+    #await bot.change_presence(status=discord.Status.online, activity=discord.Game("Type '!help' for help"))
+    print(f'Bot connected as {bot.user}')
 
 @bot.event
 async def on_message(message):
@@ -33,30 +36,43 @@ async def delete(ctx,nb_of_messages: int):
     for m in messages:
         await m.delete()
 
-"""@bot.command
+@bot.command()
 async def ban(ctx,member: discord.Member,*,reason=None):
-    await member.ban(reason=reason)
-    await ctx.send(f"Banned {member.mention}")
+    if ctx.author.name == 'Manitas':
+        await member.ban(reason=reason)
+        await ctx.send(f"Banned {member.mention}.")
+    else:
+        await ctx.send('Non Non Non' + ctx.author.mention + '.')
 
-@bot.command
+@bot.command()
 async def kick(ctx,member: discord.Member,*,reason=None):
-    await member.kick(reason=reason)
+    if ctx.author.name == 'Manitas':
+        await member.kick(reason=reason)
+        await ctx.send('Rip ' + member.mention + '.')
+    else:
+        await ctx.send(f'Laisse le bro {member.mention} tranquille tarba de ' + ctx.author.mention + '.')
 
-@bot.command
+@bot.command()
 async def unban(ctx,*,member):
-    banned_users = await ctx.guild.ban()
-    member_name , member_tag = member.split('#')
-    for banned_u in banned_users:
-        user = banned_u.user
-        if (user.name, user.discriminator) == (member_name, member_tag):
-            await ctx.guild.unban(user)
-            await ctx.send(f"Unbanned {user.mention}")
-            return"""
+    if ctx.author.name == 'Manitas':
+        banned_users = await ctx.guild.ban()
+        member_name , member_tag = member.split('#')
+        for banned_u in banned_users:
+            user = banned_u.user
+            if (user.name, user.discriminator) == (member_name, member_tag):
+                await ctx.guild.unban(user)
+                await ctx.send(f"Unbanned {user.mention}.")
+                return
+    else:
+        await ctx.send('Fait pas le malin' + ctx.author.mention + '.')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(bot.latency*1000)}ms')
 
+@tasks.loop(seconds=10)
+async def change_status():
+    await bot.change_presence(activity=discord.Game(next(status)))
 
 #@bot.command(name='server')
 #async def fetchServerInfo(ctx):
